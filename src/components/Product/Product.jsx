d782589cc263ee7cb4c4adbae53c417b90fb6262
MD. Iktedar Hasan Rushdi Rajin <RushDizPC@MDs-MacBook-Pro.local>
2023-04-13 03:24:46 +0600

    cart added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
 
 const Product = (props) => {
     const {img, name, price, seller, ratings} = props.product;
+    const addToCart = props.addToCart;
     return (
         <div className='product-container'>
             <img src= {img} alt="shoe" />
-            <h3 className='product-name'>{name}</h3>
-            <h4  className='product-price'>Price: ${price}</h4>
-            <p className='product-seller'>Manufacturer: {seller}</p>
-            <p className='product-rating'>Rating: {ratings} stars</p>
-            <button className='product-btn'>Add to cart</button>
+            <div className='product-info'>
+                <h3 className='product-name'>{name}</h3>
+                <h4  className='product-price'>Price: ${price}</h4>
+                <p className='product-seller'>Manufacturer: {seller}</p>
+                <p className='product-rating'>Rating: {ratings} stars</p>
+            </div>
+            <button onClick={()=> addToCart(props.product)} className='product-btn'>Add to cart <FontAwesomeIcon icon = {faShoppingCart} /></button>
         </div>
     );
 };
--- src/components/Shop-container/Shop.jsx
+++ src/components/Shop-container/Shop.jsx
@@ -5,6 +5,13 @@ import Product from '../Product/Product';
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
+    const [cart, setCart] = useState([]);
+    const addToCart = products =>{
+        // console.log(products);
+        let newCart = [...cart , products];
+        setCart(newCart);
+    }
+
     useEffect ( () => {
         fetch('products.json')
         .then(response => response.json())
@@ -20,13 +27,14 @@ const Shop = () => {
                 {
                     products.map(product => <Product 
                         key = {product.id} 
-                        product ={product}>
-
+                        product ={product}
+                        addToCart = {addToCart}>
                     </Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3>Order summary</h3>
+                <p>Selected items: {cart.length}</p>
             </div>
         </div>
     );
